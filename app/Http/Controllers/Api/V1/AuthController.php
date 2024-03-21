611be31a3e2e52091db4c68fffa1b7586038edd8
irfankully123 <150472671+irfankully123@users.noreply.github.com>
2024-03-22 01:51:57 +0500

    optimization

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -44,26 +44,26 @@ class AuthController extends Controller
      */
     public function login(Request $request): JsonResponse
     {
-
         $loginUserData = $request->validate([
             'email' => 'required|string|email',
-            'password' => 'required|min:8|max:20'
+            'password' => 'required|string|min:8|max:20',
         ]);
-
+    
         $user = User::where('email', $loginUserData['email'])->first();
-
+    
         if (!$user || !Hash::check($loginUserData['password'], $user->password)) {
             return response()->json([
                 'message' => 'Invalid Credentials'
-            ], ResponseAlias::HTTP_NOT_FOUND);
+            ], ResponseAlias::HTTP_UNAUTHORIZED); 
         }
-
+    
         $token = $user->createToken($user->name . '-AuthToken')->plainTextToken;
-
+    
         return response()->json([
             'token_type' => 'Bearer',
             'token' => $token
         ], ResponseAlias::HTTP_OK);
     }
+    
 
 }
